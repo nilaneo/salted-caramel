259f27765769e0a3afd01383a0b93eafc05e360a
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2018-03-12 09:04:20 +0200

    Save arrays of post ids by hashtags

--- main.mjs
+++ main.mjs
@@ -7,20 +7,38 @@ const { SALTED_CARAMEL_USERNAME, SALTED_CARAMEL_PASSWORD } = process.env;
     headless: false
   });
 
-  // 1. Open Login Page
+  // 1. Login
+  // 1.1. Open Login Page
   const page = await browser.newPage();
   await page.goto('https://www.instagram.com/accounts/login/');
 
-  // 2. Enter login
+  // 1.2. Enter login
   await page.type('input[name="username"]', SALTED_CARAMEL_USERNAME, { delay: 100 });
 
-  // 3. Enter password
+  // 1.3. Enter password
   await page.type('input[name="password"]', SALTED_CARAMEL_PASSWORD, { delay: 100 });
 
-  // 4. Submit form
+  // 1.4. Submit form
   await page.click('form button');
 
-  await sleep(5000);
+  const hashtags = [
+    'simplebeyond',
+    'vsco'
+  ];
+  // 2. For each hashtag search posts by hashtag
+  for (let hashtag of hashtags) {
+    // 2.1. Open page for hashtag
+    await page.goto(`https://www.instagram.com/explore/tags/${hashtag}`);
+
+    // 2.2. Get IDs of N most recent photos
+    // 2.2.1. Get all visible most recent items
+    const allMostRecentPhotoIds = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll('h2')[1].nextSibling.querySelectorAll('a')).filter(a => !a.querySelector('.coreSpriteVideoIconSmall')).map(a => a.href.match(/\/p\/(.+)\//)[1]);
+    });
+    // 2.2.2. Get N items from most recent items
+    const numberOfPhotosPerHashtag = 5;
+    const mostRecentPhotoIds = allMostRecentPhotoIds.slice(0, numberOfPhotosPerHashtag);
+  }
 
   await browser.close();
 })();
